63c6c030f074ecdbf8d073538de87aff32cabc1e
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-15 19:26:30 -0700

    Let people install huspenders from git HEAD

--- Formula/huspenders.rb
+++ Formula/huspenders.rb
@@ -2,6 +2,7 @@ class Huspenders < Formula
   desc "Create a Cabal-based project with sane defaults"
   homepage "https://github.com/gabebw/huspenders"
   url "https://github.com/gabebw/huspenders/archive/v0.0.1.tar.gz"
+  head "https://github.com/gabebw/huspenders.git"
   sha256 "e80cc48606853a324d8fc86387c69a2819c51dfe8620b05901046e4e8bc490ef"
 
   def install
