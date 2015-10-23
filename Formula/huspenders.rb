c777672774d7a749b90fa8430c28c33d1ebdc433
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-23 16:09:26 -0700

    huspenders 0.0.2

--- Formula/huspenders.rb
+++ Formula/huspenders.rb
@@ -1,9 +1,9 @@
 class Huspenders < Formula
   desc "Create a Cabal-based project with sane defaults"
   homepage "https://github.com/gabebw/huspenders"
-  url "https://github.com/gabebw/huspenders/archive/v0.0.1.tar.gz"
+  url "https://github.com/gabebw/huspenders/archive/v0.0.2.tar.gz"
   head "https://github.com/gabebw/huspenders.git"
-  sha256 "e80cc48606853a324d8fc86387c69a2819c51dfe8620b05901046e4e8bc490ef"
+  sha256 "c6cbe4f0132966440db17116e7a74fde09e862c88796b0a79a2af003c7186240"
 
   def install
     bin.install "huspenders"
