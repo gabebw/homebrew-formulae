fe77f404651da569025688367dd4c0eddfcc3e97
Gabe Berke-Williams <gabebw@gabebw.com>
2017-09-02 19:20:46 -0700

    most-used: version 0.0.6

--- Formula/most-used.rb
+++ Formula/most-used.rb
@@ -5,9 +5,9 @@ class MostUsed < Formula
 
   desc "Find your most-used commands"
   homepage "https://github.com/gabebw/most-used"
-  url "https://github.com/gabebw/most-used/archive/v0.0.5.tar.gz"
-  sha256 "5507534c7eb1c0f558ae845887218d02ccc9e683b3f73398c3f9521f983ce677"
-  version "0.0.5"
+  url "https://github.com/gabebw/most-used/archive/v0.0.6.tar.gz"
+  sha256 "f649dcfb5f93198b61836a60805b3b58f5dffe2974fe032b1ecfb0c7b6d51cfe"
+  version "0.0.6"
   head "https://github.com/gabebw/most-used"
 
   depends_on "ghc" => :build
