a2356bcb485996fe482aff06c31e80cb03118125
Gabe Berke-Williams <gabebw@gabebw.com>
2017-11-08 12:09:22 -0800

    Fix infinite-recursion bug in pspg

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -1,10 +1,12 @@
 class Pspg < Formula
   desc "Postgres Pager"
   homepage "https://github.com/okbob/pspg"
-  url "https://github.com/okbob/pspg/archive/0.5.tar.gz"
-  sha256 "754d1e380d072517e9bc2c3c38785e2f19a9f927f061de9a646fd1094baa204e"
+  url "https://github.com/okbob/pspg/archive/0.6.tar.gz"
+  sha256 "3b6a26ce43759eef6524230905a2b444bcbdbb133dda337d44efa05bddcce59a"
 
   def install
+    # The `--recheck` makes it re-run forever
+    inreplace "config.make.in", "./config.status --recheck", "./config.status"
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make", "install"
   end
