09e2b34517a98200c0deac358a9b246f673eaae6
Gabe Berke-Williams <gabebw@gabebw.com>
2019-09-01 12:03:33 -0700

    Candle v0.5.0

--- Formula/candle.rb
+++ Formula/candle.rb
@@ -1,10 +1,10 @@
 class Candle < Formula
-  version "0.4.0"
+  version "0.5.0"
   desc "Shine a little light on your HTML using the command line"
   homepage "https://github.com/gabebw/candle"
 
   url "https://github.com/gabebw/candle/releases/download/v#{version}/candle-#{version}.tar.gz"
-  sha256 "8c21f517b78ca8516c0dff6f55baf1f6377c1ec72a4511b7fa3a377deb2d3336"
+  sha256 "16463c834ea31acf95132c4ca55df3ae52953aaf5ea575e9809c3cd0a7a26981"
 
   def install
     bin.install "candle"
