453b48e87a2f68b0def3db275fa853f131cb310e
Gabe Berke-Williams <gabebw@gabebw.com>
2024-07-03 12:10:07 -0700

    Update candle formula for arm64
    It now handles both x86 and arm64.

--- Formula/candle.rb
+++ Formula/candle.rb
@@ -1,10 +1,19 @@
 class Candle < Formula
-  version "0.8.0"
+  version "0.9.0"
   desc "Shine a little light on your HTML using the command line"
   homepage "https://github.com/gabebw/candle"
 
-  url "https://github.com/gabebw/candle/releases/download/v#{version}/candle-#{version}.tar.gz"
-  sha256 "a0f0ae2d7938fb4b3dc732ac26bc1afb27f066dd554436c5d69f8537dc018300"
+  on_macos do
+    on_intel do
+      url "https://github.com/gabebw/candle/releases/download/v#{version}/candle-#{version}-x86_64.tar.gz"
+      sha256 "a2716b2ea9ea31c4b267d08f90ff62386a0baf1593897ae7ea28f64ccd3f4db1"
+    end
+
+    on_arm do
+      url "https://github.com/gabebw/candle/releases/download/v#{version}/candle-#{version}-aarch64.tar.gz"
+      sha256 "624a49e5598d65857afc37110670a3b1ae37c79a1579bcd1a316faffdf7c1383"
+    end
+  end
 
   def install
     bin.install "candle"
